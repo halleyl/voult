12f3a0db603b045d9193b8e2234bc20bd5a7d74d
halleyl <halleyl@uw.edu>
2018-12-14 11:54:47 -0800

    Add Bootstrap carousel to Clients page

--- script/menu.js
+++ script/menu.js
@@ -1,12 +1,14 @@
 var hamburger = document.querySelector('.hamburger')
-var menu = document.querySelector('ul:first-of-type')
+var menu = document.querySelector('ul')
 
 hamburger.addEventListener('click',function() {
   if (menu.classList.contains('hidden')) {
     menu.classList.remove('hidden')
     menu.classList.add('shown')
+    hamburger.setAttribute('src','images/menu-x.svg')
   } else if (menu.classList.contains('shown')) {
     menu.classList.remove('shown')
     menu.classList.add('hidden')
+    hamburger.setAttribute('src','images/menu.svg')
   }
 })
