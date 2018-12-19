c9bf5cdfad1eb14b06dcf70a2460f1648acb50f8
halleyl <halleyl@uw.edu>
2018-12-19 13:17:03 -0800

    Add breakpoints/flexbox for subpage content, adjust index page client image sizes

---
+++ script/clients.js
@@ -0,0 +1,5 @@
+To use JS to skip to specific slide:
+https://stackoverflow.com/questions/25663459/different-links-to-different-bootstrap-carousel-slides-in-one-page
+
+This comment may be most helpful:
+https://stackoverflow.com/a/25680261
